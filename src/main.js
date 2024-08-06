7d1225d70192fc5d79077562bdba4cebde777405
Chaley Boatmon <chaleyboatmon@gmail.com>
2024-08-06 14:16:24 -0400

    import fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './styles/main.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { createApp } from 'vue';
 import App from './App.vue';
